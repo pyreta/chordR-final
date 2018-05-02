5f8d0e2136a347b5eb10bb4f0f66df31e33d94ae
Louis Abramson <pyreta@gmail.com>
2018-05-02 11:23:22 -0400

    add gist documentation in help menu

--- app/menu.js
+++ app/menu.js
@@ -92,10 +92,7 @@ export default class MenuBuilder {
     const subMenuHelp = {
       label: 'Help',
       submenu: [
-        { label: 'Learn More', click() { shell.openExternal('http://electron.atom.io'); } },
-        { label: 'Documentation', click() { shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme'); } },
-        { label: 'Community Discussions', click() { shell.openExternal('https://discuss.atom.io/c/electron'); } },
-        { label: 'Search Issues', click() { shell.openExternal('https://github.com/atom/electron/issues'); } }
+        { label: 'Learn More', click() { shell.openExternal('https://gist.github.com/pyreta/87237681f1fd19075b29629e46889e21'); } },
       ]
     };
 
